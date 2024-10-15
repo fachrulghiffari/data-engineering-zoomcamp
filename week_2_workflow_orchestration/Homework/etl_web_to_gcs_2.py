51e0733d874b6b509a01a51ba983cb4e05bf6a29
Muhammad Fachrul Ghiffari <104657138+fachrulghiffari@users.noreply.github.com>
2024-10-15 17:29:12 +0700

    Update etl_web_to_gcs_2.py

--- week_2_workflow_orchestration/Homework/etl_web_to_gcs_2.py
+++ week_2_workflow_orchestration/Homework/etl_web_to_gcs_2.py
@@ -32,7 +32,7 @@ def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path :
     return path
 
 @task()
-def write_gcs(path: Path, color: str) -> None:
+def write_gcs(path: Path, color: str, dataset_file: str) -> None:
     """Upload local parquet file to GCS"""
 
     gcs_block = GcsBucket.load("zoom-gcs")
@@ -52,7 +52,7 @@ def etl_web_to_gcs() -> None:
     df = fetch(dataset_url)
     df_clean = clean(df)
     path = write_local(df_clean, color, dataset_file)
-    write_gcs(path, color)
+    write_gcs(path, color, dataset_file)
 
 if __name__ == '__main__':
     etl_web_to_gcs()
