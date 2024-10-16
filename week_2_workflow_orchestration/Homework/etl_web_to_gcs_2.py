33bfefc6188e7c461e7c8d1d0aa9c3ceda944f0f
Muhammad Fachrul Ghiffari <104657138+fachrulghiffari@users.noreply.github.com>
2024-10-16 11:04:49 +0700

    Update etl_web_to_gcs_2.py

--- week_2_workflow_orchestration/Homework/etl_web_to_gcs_2.py
+++ week_2_workflow_orchestration/Homework/etl_web_to_gcs_2.py
@@ -45,7 +45,7 @@ def etl_web_to_gcs() -> None:
     """The main ETL function"""
     color = "green"
     year = 2019
-    month = 3
+    month = 4
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url=f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
 
